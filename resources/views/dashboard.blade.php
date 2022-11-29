d0ed153d52aad736b0c2fc2e485f8801a03f34bf
ishan058 <ishankhadkaik246@gmail.com>
2022-11-29 11:29:35 +0545

    oka

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <h1>Welcome to Dashboard</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
+    <!-- <button type="button" class="btn btn-primary">Primary</button>
     <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-success">Success</button> -->
     <div class="container">
         <form method="get" action="{{route('logout')}}">
             <button type="submit" class="btn btn-primary">Logout</button>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -17,7 +17,7 @@
                 <div class="invalid-feedback">
                     {{$message}}
                 </div>
-                @enderror
+            @enderror
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -26,7 +26,7 @@
                 <div class="invalid-feedback">
                     {{$message}}
                 </div>
-                @enderror
+            @enderror
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
